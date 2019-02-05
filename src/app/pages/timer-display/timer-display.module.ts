11d2100ea1bbd351b21a8a56ad0b937c694355f2
Marc Kassay <marckassay@gmail.com>
2019-02-05 13:59:15 -0500

    feat(timer-display) add

--- src/app/pages/timer-display/timer-display.module.ts
+++ src/app/pages/timer-display/timer-display.module.ts
@@ -1,25 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { FabContainerComponentModule } from 'src/app/components/fab-container/fab-container.module';
+
+import { DisplayPageResolverService } from '../display-page-router.service';
+import { TimerSettingsPageModule } from '../timer-settings/timer-settings.module';
+import { TimerSettingsPage } from '../timer-settings/timer-settings.page';
 
 import { TimerDisplayPage } from './timer-display.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: TimerDisplayPage
+    path: ':id',
+    component: TimerDisplayPage,
+    resolve: { subject: DisplayPageResolverService }
   }
 ];
 
 @NgModule({
+  entryComponents: [TimerSettingsPage],
   imports: [
     CommonModule,
     FormsModule,
+    FabContainerComponentModule,
+    TimerSettingsPageModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [TimerDisplayPage]
+  exports: [RouterModule],
+  declarations: [TimerDisplayPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TimerDisplayPageModule { }
